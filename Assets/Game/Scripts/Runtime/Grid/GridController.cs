d57898ab5d6d002659134197d281e7076ab0487b
Dave Roxas <roxasdavelouie@yahoo.com>
2025-10-23 00:10:25 +0800

    added Grid controllers

---
+++ Assets/Game/Scripts/Runtime/Grid/GridController.cs
@@ -0,0 +1,19 @@
+﻿namespace Game.Scripts.Runtime.Factory
+{
+    public class GridController
+    {
+        public ICell[,] Grid { get; private set; }
+
+        private int _width = 0;
+        private int _height = 0;
+
+        public GridController(int width, int height, IGridFactory factory)
+        {
+            _width = width;
+            _height = height;
+            Grid = factory.Create(width, height);
+        }
+        
+        
+    }
+}
