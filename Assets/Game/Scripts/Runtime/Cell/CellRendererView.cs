5d905ac43c4b746a806384b9e523162818add14c
Dave Roxas <roxasdavelouie@yahoo.com>
2025-10-22 17:59:21 +0800

    Added cell prefab and Cell renderers

---
+++ Assets/Game/Scripts/Runtime/Cell/CellRendererView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Scripts.Runtime.Cell
+{
+    public class CellRendererView : MonoBehaviour, ICellRenderer
+    {
+        [Header(StrattonConstants.INSPECTOR.REFERENCES)]
+        [SerializeField]
+        private SpriteRenderer spriteRenderer;
+        
+        private ICell localCell;
+        private Color aliveColor;
+        private Color deadColor;
+        
+        public void Initialize(ICell cell, Color alive, Color dead)
+        {
+            localCell = cell;
+            localCell.OnStateChanged += OnCellStateChanged;
+            aliveColor = alive;
+            deadColor = dead;
+            
+            // initial setup
+            OnCellStateChanged(cell);
+        }
+
+        private void OnDestroy()
+        {
+            localCell.OnStateChanged += OnCellStateChanged;
+        }
+
+        private void OnCellStateChanged(ICell obj)
+        {
+            // spriteRenderer.color = obj.IsAlive ? aliveColor : deadColor;
+            gameObject.SetActive(obj.IsAlive);
+        }
+
+        public void SetPosition(float x, float y)
+        {
+            transform.localPosition = new Vector3(x, y, 0);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Runtime/Cell/ICellRenderer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Runtime.Cell
+{
+    public interface ICellRenderer
+    {
+        void Initialize(ICell cell, Color alive, Color dead);
+        void SetPosition(float x, float y);
+    }
+}
