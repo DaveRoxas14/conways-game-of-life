6324f8035909ca86e97fbd34f597c5b9ad718bae
Dave Roxas <roxasdavelouie@yahoo.com>
2025-10-22 16:48:47 +0800

    Added Game manager initial script

---
+++ Assets/Game/Scripts/Runtime/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Scripts.Runtime
+{
+    public class GameManager : MonoBehaviour
+    {
+        [Header(StrattonConstants.INSPECTOR.GRID)] 
+        [SerializeField] private int _gridWidth = 30;
+        [SerializeField] private int _gridHeight = 10;
+        [SerializeField] private float _interval = 0.1f;
+        [SerializeField] private Color _aliveColor = Color.green;
+        [SerializeField] private Color _deadColor = Color.red;
+
+        [Header(StrattonConstants.INSPECTOR.REFERENCES)] 
+        [SerializeField] private GameObject _cellPrefab;
+        [SerializeField] private Transform _cellParent;
+
+        private void Start()
+        {
+            
+        }
+
+        private void Next()
+        {
+            
+        }
+    }
+}
