df69716eaea13c30f2464a6045ca754847073d59
Dave Roxas <roxasdavelouie@yahoo.com>
2025-10-22 16:48:11 +0800

    Added Constants

---
+++ Assets/Game/Scripts/Runtime/StrattonConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace Game.Scripts.Runtime
+{
+    public static class StrattonConstants
+    {
+
+        public static class INSPECTOR
+        {
+            public const string REFERENCES = "References";
+            public const string GRID = "Grid";
+            public const string SETTINGS = "Settings";
+            public const string DEBUG = "Debug";
+        }
+        
+        public static class MENU_NAME
+        {
+            
+        }
+        
+    }
+}
