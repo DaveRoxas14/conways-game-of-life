6196eaea94faae08bd9874a565e1488ab8754ba2
Dave Roxas <roxasdavelouie@yahoo.com>
2025-10-22 17:41:56 +0800

    Added input handlers
    - also added input reader that will communicate to InputActions

---
+++ Assets/Game/Scripts/Runtime/Input/InputHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Scripts.Runtime.Input
+{
+    public class InputHandler : MonoBehaviour
+    {
+        [Header(StrattonConstants.INSPECTOR.REFERENCES)]
+        [SerializeField] private Camera _camera;
+
+        private void Start()
+        {
+            
+        }
+
+        private void OnLeftMouseClick()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Runtime/Input/InputReader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Scripts.Runtime.Input
+{
+    public class InputReader : ScriptableObject
+    {
+        public event Action OnClick;
+    }
+}
