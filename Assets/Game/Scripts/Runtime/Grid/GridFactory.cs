72173e480a7fac58670a148c860122a4914f2a3f
Dave Roxas <roxasdavelouie@yahoo.com>
2025-10-23 00:11:11 +0800

    fixed issues on grid factory

--- Assets/Game/Scripts/Runtime/Grid/GridFactory.cs
+++ Assets/Game/Scripts/Runtime/Grid/GridFactory.cs
@@ -1,13 +1,13 @@
 ﻿namespace Game.Scripts.Runtime.Factory
 {
-    public class GridFactory :IGridFactory
+    public class GridFactory : IGridFactory
     {
         public ICell[,] Create(int width, int height)
         {
             var grid = new ICell[height, width];
-            for (var x = 0; x < height; x++)
+            for (var x = 0; x < width; x++)
             {
-                for (var y = 0; y < width; y++)
+                for (var y = 0; y < height; y++)
                 {
                     grid[x, y] = new Cell.Cell(x, y, false);
                 }
